71982c2862e746b2a4e9c9c28e306967277c172c
An Nuyen <annuyen@gmail.com>
2018-03-13 19:10:22 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :show] 
-  
+  resources :users, only: [:new, :create, :show]
+
   get '/login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
 
