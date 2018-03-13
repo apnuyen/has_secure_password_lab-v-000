e820708a3b3f755a4564398183a235c0529acbaa
An Nuyen <annuyen@gmail.com>
2018-03-13 19:10:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:new, :create, :show]
-
-  get '/login', to: 'sessions#new', as 'login'
+  resources :users, only: [:new, :create, :show] 
+  
+  get '/login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
